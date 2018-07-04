d004723041bbaa1ccbc07aa551cde1b829f99298
usagisagi <hiroito48@gmail.com>
2018-07-04 21:22:30 +0900

    unpool

--- unpool.py
+++ unpool.py
@@ -1,11 +1,16 @@
+from typing import Tuple
+
 import tensorflow as tf
+from tensorflow import TensorShape, Tensor
+
+import numpy as np
 
 
-def pooling(input,
-            ksize=tf.constant([1, 2, 2, 1]),
-            strides=tf.constant([1, 2, 2, 1]),
-            padding='VALID',
-            name='pooling'):
+def pooling(input: Tensor,
+            ksize,
+            strides,
+            padding: str = 'VALID',
+            name: str = 'pooling') -> Tuple[Tensor, Tensor, TensorShape]:
     """
     pooling層。実質pooling_with_arg_max。
 
@@ -20,10 +25,14 @@ def pooling(input,
     with tf.name_scope(name):
         input_shape = input.get_shape()
         output, argmax = \
-            tf.nn.max_pool_with_argmax(input, ksize, strides, padding, Targmax=tf.int32)
+            tf.nn.max_pool_with_argmax(input, ksize, strides, padding)
         return output, argmax, input_shape
 
-def unpooling(input, argmax, output_shape, name):
+
+def unpooling(input: Tensor,
+              argmax: Tensor,
+              output_shape: TensorShape,
+              name='unpooling'):
     """
     unpooling層
 
@@ -35,5 +44,33 @@ def unpooling(input, argmax, output_shape, name):
     """
 
     with tf.name_scope(name):
-        # inputを1次元にする
-        tf.reshape(input, [-1])
+        # inputを直線に並べる
+        input_stride = tf.reshape(input, [-1])
+
+        # argmaxを直線に並べる。1行1要素の2次元になる。
+        argmax_stride = tf.reshape(argmax, [-1, 1])
+
+        # outputの要素数
+        num_elem = output_shape.num_elements()
+
+        # inputをargmaxに従い、num_elemの1行Tensorに再配置
+        output_stride = tf.scatter_nd(argmax_stride, input_stride, [num_elem])
+
+        output = tf.reshape(output_stride, output_shape)
+
+        return output
+
+
+if __name__ == '__main__':
+    input = tf.constant(np.arange(16).reshape(1, 4, 4, 1))
+    init = tf.global_variables_initializer()
+    pooled, argmax, shape = pooling(input, [1, 2, 2, 1], [1, 2, 2, 1])
+    unpooled = unpooling(pooled, argmax, shape)
+    unpooled_view = tf.reshape(unpooled, (4, 4))
+
+    writer = tf.summary.FileWriter("./logdir/", tf.get_default_graph())
+    with tf.Session() as sess:
+        init.run()
+        print(unpooled_view.eval())
+
+    writer.close()
